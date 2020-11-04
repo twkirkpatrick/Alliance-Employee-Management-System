66fff48ccb95cacfe6ff0a00dc6322c799c699c0
Tanner Kirkpatrick <twk4491@gmail.com>
2020-11-04 11:47:42 -0500

    initial MySQL connection setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Slowdive1!",
+  database: "alliance_DB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+});
