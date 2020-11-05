d84921185d8e10bc6f4dd8c72ad1b4d9dbcdd586
Tanner Kirkpatrick <twk4491@gmail.com>
2020-11-05 14:31:44 -0500

    re-structured SQL file for seamless transition of data into database

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,6 @@ CREATE DATABASE alliance_DB;
 
 USE alliance_DB;
 
-CREATE TABLE employees (
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
-  PRIMARY KEY(id),
-  FOREIGN KEY(role_id) REFERENCES roles(id),
-  FOREIGN KEY(manager_id) REFERENCES employee(id)
-);
-
 CREATE TABLE departments(
     id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(30),
@@ -29,6 +18,41 @@ CREATE TABLE roles (
     PRIMARY KEY(id),
     FOREIGN KEY(department_id) REFERENCES department(id)
 );
+
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(role_id) REFERENCES roles(id),
+  FOREIGN KEY(manager_id) REFERENCES employees(id)
+);
+
+
+
+
+INSERT INTO departments (dept_name)
+VALUES ("Development"), ("Sales"), ("Marketing");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+("Tech Lead", 150000, 1),
+("Full Stack Developer", 800000, 1), 
+("Front End Developer", 75000, 1),
+("UX/UI Designer", 600000, 1),
+
+("Director of Sales", 115000, 2),
+("Area Manager", 90000, 2),
+("Account Manager", 65000, 2),
+("Inside Sales", 50000, 2),
+
+("Director of Marketing", 120000, 3),
+("Account Strategist", 90000, 3),
+("SEO Analyst", 75000, 3),
+("SEM Analyst", 70000, 3);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
@@ -45,28 +69,18 @@ VALUES
 ("Tony", "Bishop", 9, null),
 ("Polly", "Gump", 10, 9),
 ("Virginia", "Wilson", 11, 9),
-("Chad", "Connor", 12, 9),
-
-INSERT INTO roles (title, salary, department_id)
-VALUES 
-("Tech Lead", "150,000", 1),
-("Full Stack Developer", "80,0000", 1), 
-("Front End Developer", "75,000", 1),
-("UX/UI Designer", "60,0000", 1),
-
-("Director of Sales", "115,000", 2),
-("Area Manager", "90,000", 2),
-("Account Manager", "65,000", 2),
-("Inside Sales", "50,000", 2),
-
-("Director of Marketing", "120,000", 3),
-("Account Strategist", "90,000", 3),
-("SEO Analyst", "75,000", 3),
-("SEM Analyst", "70,000", 3),
+("Chad", "Connor", 12, 9);
 
 
-INSERT INTO departments (dept_name)
-VALUES ("Development"), ("Sales"), ("Marketing")
+
+
+Error Code: 1366. Incorrect decimal value: '150,000' for column 'salary' at row 1
+
+Error Code: 1366. Incorrect decimal value: '150,000.00' for column 'salary' at row 1
+Error Code: 1136. Column count doesn't match value count at row 1
+
+
+Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`alliance_db`.`employees`, CONSTRAINT `employees_ibfk_2` FOREIGN KEY (`manager_id`) REFERENCES `employees` (`id`))
 
 
 
